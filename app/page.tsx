0cc2dfa81bba39c26b9fdd548a15def930932d57
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2025-01-22 23:48:06 -0300

    feat: total visits

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from './components/landing-page/hero'
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto px-8">
       <Hero />
       {/* <Header/>
       <Hero/>
