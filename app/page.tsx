4cdd3428aebc0638820ad90cb54043eff8c91308
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2025-01-24 23:13:59 -0300

    feat: header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { Header } from './components/landing-page/header'
 import Hero from './components/landing-page/hero'
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto px-8">
+      <Header />
       <Hero />
-      {/* <Header/>
-      <Hero/>
+      {/* <Hero/>
       <VideoExplanation/>
       <Pricing/>
       <Faq/> */}
