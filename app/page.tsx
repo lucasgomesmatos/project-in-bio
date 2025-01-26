ee16beb4bec15d15b5fce5b4a0965ac5d2bc6e8c
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2025-01-25 23:27:00 -0300

    feat: add video and pricing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { Header } from './components/landing-page/header'
 import Hero from './components/landing-page/hero'
+import { Pricing } from './components/landing-page/pricing'
+import { VideoExplanation } from './components/landing-page/video-explanation'
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto px-8">
       <Header />
       <Hero />
+      <VideoExplanation />
+      <Pricing />
       {/* <Hero/>
       <VideoExplanation/>
       <Pricing/>
